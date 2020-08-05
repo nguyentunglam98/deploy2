05b19c0a8740795bcb32f488885f184968e09b2b
DESKTOP-NEVOTAS\lenovo <lamntse05790@fpt.edu.vn>
2020-08-05 21:01:20 +0700

    lamnt98 commit add transactional 05/08

--- project/src/main/java/com/example/webDemo3/repository/NewsletterRepository.java
+++ project/src/main/java/com/example/webDemo3/repository/NewsletterRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.List;
  * lamnt98
  * 27/07
  */
+@Transactional
 @Repository
 public interface NewsletterRepository extends JpaRepository<Newsletter,Integer> {
     @Query(value = "select n from Newsletter n where n.status = 0 order by n.gim desc")

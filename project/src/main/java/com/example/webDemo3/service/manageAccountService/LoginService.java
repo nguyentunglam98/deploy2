300c6f67829ef36f507a633fc6ff17f6d06f6139
DESKTOP-NEVOTAS\lenovo <lamntse05790@fpt.edu.vn>
2020-08-10 11:37:13 +0700

    lam delete all 10/08

--- project/src/main/java/com/example/webDemo3/service/manageAccountService/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.example.webDemo3.service.manageAccountService;
-
-import com.example.webDemo3.dto.manageAccountResponseDto.LoginResponseDto;
-import com.example.webDemo3.dto.request.manageAccountRequestDto.LoginRequestDto;
-
-public interface LoginService {
-    LoginResponseDto checkLoginUser(LoginRequestDto u);
-}

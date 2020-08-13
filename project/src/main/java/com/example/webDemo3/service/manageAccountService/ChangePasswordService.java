60353166d7ecb41a20f3c3eb0397360e78c6c925
DESKTOP-NEVOTAS\lenovo <lamntse05790@fpt.edu.vn>
2020-08-13 15:09:07 +0700

    delete customer

--- project/src/main/java/com/example/webDemo3/service/manageAccountService/ChangePasswordService.java
+++
@@ -1,8 +0,0 @@
-package com.example.webDemo3.service.manageAccountService;
-
-import com.example.webDemo3.dto.MessageDTO;
-import com.example.webDemo3.dto.request.manageAccountRequestDto.ChangePasswordRequestDto;
-
-public interface ChangePasswordService {
-    MessageDTO checkChangePasswordUser(ChangePasswordRequestDto user);
-}
--- project/src/main/java/com/example/webDemo3/service/manageSchoolRank/AdditionFunctionSchoolRankService.java
+++
@@ -1,9 +0,0 @@
-package com.example.webDemo3.service.manageSchoolRank;
-
-import java.sql.Date;
-
-public interface AdditionFunctionSchoolRankService {
-    String addHistory(String oldhistory, String userName, Date date);
-    Date convertDateInComputerToSqlDate();
-    String convertSqlDateToString(Date date);
-}

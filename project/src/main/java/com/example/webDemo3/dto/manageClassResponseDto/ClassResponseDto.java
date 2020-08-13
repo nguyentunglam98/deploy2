97b51e31d4afb0b6faaded59afb93e29066de261
DESKTOP-NEVOTAS\lenovo <lamntse05790@fpt.edu.vn>
2020-08-13 20:33:16 +0700

    delete all 13/08

--- project/src/main/java/com/example/webDemo3/dto/manageClassResponseDto/ClassResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.webDemo3.dto.manageClassResponseDto;
-
-import lombok.Data;
-
-@Data
-public class ClassResponseDto {
-    private Integer classID;
-    private String className;
-}

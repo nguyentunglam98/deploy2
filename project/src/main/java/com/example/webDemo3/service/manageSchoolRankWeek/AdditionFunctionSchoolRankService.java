b24c804b11f9532b9e4f30b60d2da241ce2f4bdd
DESKTOP-NEVOTAS\lenovo <lamntse05790@fpt.edu.vn>
2020-08-23 00:41:11 +0700

    delete all

--- project/src/main/java/com/example/webDemo3/service/manageSchoolRankWeek/AdditionFunctionSchoolRankService.java
+++
@@ -1,9 +0,0 @@
-package com.example.webDemo3.service.manageSchoolRankWeek;
-
-import java.sql.Date;
-
-public interface AdditionFunctionSchoolRankService {
-    String addHistory(String oldhistory, String userName, Date date);
-    Date convertDateInComputerToSqlDate();
-    String convertSqlDateToString(Date date);
-}

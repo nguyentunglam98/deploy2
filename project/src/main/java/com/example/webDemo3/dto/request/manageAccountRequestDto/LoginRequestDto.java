13e40e3e943dfeded51dc0095a97aa190575d47b
DESKTOP-NEVOTAS\lenovo <lamntse05790@fpt.edu.vn>
2020-08-23 12:59:10 +0700

    delete all 23/08

--- project/src/main/java/com/example/webDemo3/dto/request/manageAccountRequestDto/LoginRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.webDemo3.dto.request.manageAccountRequestDto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequestDto {
-    private String username;
-    private String password;
-}
--- project/src/main/java/com/example/webDemo3/service/manageEmulationService/ViolationHistoryService.java
+++
@@ -1,8 +0,0 @@
-package com.example.webDemo3.service.manageEmulationService;
-
-import com.example.webDemo3.dto.manageEmulationResponseDto.ViewViolationClassListResponseDto;
-import com.example.webDemo3.dto.request.manageEmulationRequestDto.ViolationHistoryResquestDTO;
-
-public interface ViolationHistoryService {
-    public ViewViolationClassListResponseDto getHistoryViolationOfClas(ViolationHistoryResquestDTO model);
-}

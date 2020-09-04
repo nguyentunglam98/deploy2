37906ce56b076bd1d77ae92745ba30bde55d2897
DESKTOP-NEVOTAS\lenovo <lamntse05790@fpt.edu.vn>
2020-09-04 23:34:14 +0700

    deploy cho loan 04/09

--- project/src/main/resources/static/js/managePost/createPost.js
+++ project/src/main/resources/static/js/managePost/createPost.js
@@ -1,28 +1,32 @@
 /*Valude default*/
 var roleID = localStorage.getItem("roleID");
 var username = localStorage.getItem("username");
+// var editor = CKEDITOR.replace('post-editor-text-content', {
+//     cloudServices_uploadUrl: 'https://73999.cke-cs.com/easyimage/upload/',
+//     cloudServices_tokenUrl: 'https://73999.cke-cs.com/token/dev/26d99879d9d20ba5d60497fc1556aa7f821ea78b8cc4c0df6f9f056e7b4e',
+//     width: '100%',
+//     height: 500,
+//     extraPlugins: 'easyimage',
+// });
 var editor = CKEDITOR.replace('post-editor-text-content', {
-    cloudServices_uploadUrl: 'https://73999.cke-cs.com/easyimage/upload/',
-    cloudServices_tokenUrl: 'https://73999.cke-cs.com/token/dev/26d99879d9d20ba5d60497fc1556aa7f821ea78b8cc4c0df6f9f056e7b4e',
-    width: '100%',
     height: 500,
-    extraPlugins: 'easyimage',
+    width: '100%',
 });
-var imageCover = CKEDITOR.replace('imageCover', {
-    cloudServices_uploadUrl: 'https://73999.cke-cs.com/easyimage/upload/',
-    cloudServices_tokenUrl: 'https://73999.cke-cs.com/token/dev/26d99879d9d20ba5d60497fc1556aa7f821ea78b8cc4c0df6f9f056e7b4e',
-    width: 250,
-    height: 200,
-    extraPlugins: 'easyimage',
-    removePlugins: 'image',
-    removeDialogTabs: 'link:advanced',
-    toolbar: [
-        {
-            name: 'insert',
-            items: ['EasyImageUpload']
-        }
-    ],
-});
+// var imageCover = CKEDITOR.replace('imageCover', {
+//     cloudServices_uploadUrl: 'https://73999.cke-cs.com/easyimage/upload/',
+//     cloudServices_tokenUrl: 'https://73999.cke-cs.com/token/dev/26d99879d9d20ba5d60497fc1556aa7f821ea78b8cc4c0df6f9f056e7b4e',
+//     width: 250,
+//     height: 200,
+//     extraPlugins: 'easyimage',
+//     removePlugins: 'image',
+//     removeDialogTabs: 'link:advanced',
+//     toolbar: [
+//         {
+//             name: 'insert',
+//             items: ['EasyImageUpload']
+//         }
+//     ],
+// });
 
 if (roleID == 1) {
     $('.form-check').removeClass('hide');
@@ -31,8 +35,8 @@ if (roleID == 1) {
 /*Save button*/
 $('#savePost').on('click', function () {
     var titleName = $('#titleName').val().trim();
-    // var image = $('#imagePreview').attr('src');
-    var image = imageCover.getData();
+    var image = $('#imagePreview').attr('src');
+    // var image = imageCover.getData();
     var data = editor.getData();
     var gim;
     if ($('input[type="checkbox"]').prop("checked") == true) {
@@ -46,14 +50,14 @@ $('#savePost').on('click', function () {
     } else if (image.trim() == "") {
         $('.createPost-err').text('Hãy nhập ảnh bìa của bài viết.');
         return false;
-    } else if (!image.includes('src=')) {
-        $('.createPost-err').text('Ảnh bìa của bài viết không đúng định dạng.');
-        return false;
+    // } else if (!image.includes('src=')) {
+    //     $('.createPost-err').text('Ảnh bìa của bài viết không đúng định dạng.');
+    //     return false;
     } else if (data == "") {
         $('.createPost-err').text('Hãy nhập nội dung của bài viết.');
         return false;
     } else {
-        image = image.split('src=')[1].split('"')[1];
+        // image = image.split('src=')[1].split('"')[1];
         var request = {
             username: username,
             header: titleName,
@@ -113,55 +117,46 @@ function addNewPost(request) {
 
 /*Upload image*/
 var loadFile = function (event) {
-    var form = $('#form-post');
-    var formData = new FormData(form[0]);
-    // formData.append('ckCsrfToken', 'xaTDx3QM1m3tTc3Uk4OYgkqgd0g1ZtcEbfhQod2a7s2rxMSzf9RhlXjMJVSV');
-    var CSTimestamp = Date.now();
-    const apiSecret = 'xaTDx3QM1m3tTc3Uk4OYgkqgd0g1ZtcEbfhQod2a7s2rxMSzf9RhlXjMJVSV';
-    $.ajax({
-        type: "POST",
-        url: "https://73999.cke-cs.com/api/v4/RQAfsTJxHLsH61eo6z1M/editors",
-        data: formData,
-        async: false,
-        headers: {
-            'X-CS-Signature': generateSignature(apiSecret, "POST", "https://73999.cke-cs.com/api/v4/RQAfsTJxHLsH61eo6z1M/editors", CSTimestamp, {
-                bundle: formData,
-                config: {
-                    cloudServices: "2019-11-30-build-f4a4c2"
-                }
-            }),
-            'X-CS-Timestamp': CSTimestamp
-        },
-        beforeSend: function () {
-            $('body').addClass("loading")
-        },
-        complete: function (resp) {
-            $('body').removeClass("loading");
-            // temp1[0].object["a"].authorization
-        },
-        success: function (data) {
-            $('#imagePreview').prop('src', data.default);
-            $('#imagePreview').prop('alt', 'Ảnh bìa bài viết');
-        },
-        failure: function (errMsg) {
-            messageModal('overrideSuccess', 'img/img-error.png', errMsg);
-        },
-        cache: false,
-        contentType: false,
-        processData: false,
-    });
-};
-
-function generateSignature(apiSecret, method, uri, timestamp, body) {
-    const url = url.parse(uri).path;
-
-    const hmac = crypto.createHmac('SHA256', apiSecret);
-
-    hmac.update(`${method.toUpperCase()}${url}${timestamp}`);
-
-    if (body) {
-        hmac.update(Buffer.from(JSON.stringify(body)));
+    var file = event.target.files[0];
+    var output = $('#imagePreview');
+    output.attr('src', URL.createObjectURL(file));
+    output.prop('alt', 'Ảnh bìa bài viết');
+    output.onload = function () {
+        URL.revokeObjectURL(output.src);
     }
+    // var CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/hnm0yiigx/upload';
+    // var CLOUDINARY_UPLOAD_PRESET = 'mrpq6qtl';
+    // var file = event.target.files[0]
+    // var formData = new FormData();
+    // formData.append('file', file);
+    // formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
 
-    return hmac.digest('hex');
-}
+    // $.ajax({
+    //     type: "POST",
+    //     url: 'https://api.cloudinary.com/v1_1/hnm0yiigx/upload',
+    //     data: formData,
+    //     headers: {
+    //         'Content-Type': 'application/x-www-form-urlencoded',
+    //         'Access-Control-Allow-Origin': '*',
+    //     },
+    //     crossDomain: true,
+    //     skipAuthorization: true,
+    //     beforeSend: function () {
+    //         $('body').addClass("loading")
+    //     },
+    //     complete: function () {
+    //         $('body').removeClass("loading");
+    //     },
+    //     success: function (data) {
+    //         $('#imagePreview').prop('src', data.url);
+    //         $('#imagePreview').prop('alt', 'Ảnh bìa bài viết');
+    //         console.log(data)
+    //     },
+    //     failure: function (errMsg) {
+    //         messageModal('overrideSuccess', 'img/img-error.png', errMsg);
+    //     },
+    //     cache: false,
+    //     contentType: false,
+    //     processData: false,
+    // });
+};
